dd6216b753c05e97f52ec0c856957709090f04c2
George.Koukounis <George.Koukounis@windsorbrokers.com>
2024-03-29 10:05:16 +0200

    Add linearGradient with box decoration

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.purple,
-          body: Center(
-            child: HomePage(),
+          body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color.fromARGB(255, 57, 0, 155), Color.fromARGB(255, 47, 0, 55)],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              )
+            ),
+            child: const Center(
+              child: HomePage(),
+            ),
           )),
     ),
   );
