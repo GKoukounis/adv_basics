6281c10583aa2e5df554ee47c9fdaa4fa629c257
George.Koukounis <George.Koukounis@windsorbrokers.com>
2024-03-29 14:39:28 +0200

    Make button as a separated widget

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,19 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({ Key? key }) : super(key: key);
+  const QuestionsScreen({Key? key}) : super(key: key);
 
   @override
   _QuestionsScreenState createState() => _QuestionsScreenState();
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  final void Function() onTap = () {};
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text('Questions Screen'),
-      )
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(questions[0].question,
+              style: const TextStyle(fontSize: 18, color: Colors.white)),
+          const SizedBox(height: 40),
+          for (var i = 0; i < questions[0].answers.length; i++)
+            QuestionButton(onTap: onTap, i: i),
+                
+        ],
+      ),
+    );
+  }
+}
+
+class QuestionButton extends StatelessWidget {
+  const QuestionButton({
+    super.key,
+    required this.onTap,
+    required this.i,
+  });
+
+  final void Function() onTap;
+  final int i;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+          onPressed: onTap,
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+              backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+              foregroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              )),
+          child: Text(questions[0].answers[i])),
     );
   }
 }
